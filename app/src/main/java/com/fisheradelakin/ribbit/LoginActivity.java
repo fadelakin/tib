0c4cb9c645850081b17a070044bd7faf80c5306d
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-02-23 00:51:44 -0500

    add ability to send messages. also some refactoring

--- app/src/main/java/com/fisheradelakin/ribbit/LoginActivity.java
+++ app/src/main/java/com/fisheradelakin/ribbit/LoginActivity.java
@@ -5,11 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +13,6 @@ import android.widget.TextView;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -93,28 +88,5 @@ public class LoginActivity extends ActionBarActivity {
                 }
             }
         });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/fisheradelakin/ribbit/RecipientsActivity.java
@@ -0,0 +1,237 @@
+package com.fisheradelakin.ribbit;
+
+import android.app.AlertDialog;
+import android.net.Uri;
+import android.support.v4.app.ListFragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RecipientsActivity extends ActionBarActivity {
+
+    // TODO: move listview and listview click listener to activity and rework things
+
+    protected Uri mMediaUri;
+    protected String mFileType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipients);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new RecipientsFragment())
+                    .commit();
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        mMediaUri = getIntent().getData();
+        mFileType = getIntent().getExtras().getString(ParseConstants.KEY_FILE_TYPE);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_recipients, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_send) {
+            ParseObject message = createMessage();
+            if(message == null) {
+                // error
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(getString(R.string.error_selecting_file))
+                        .setTitle(getString(R.string.error_selecting_file_title))
+                        .setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                send(message);
+                finish();
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void send(ParseObject message) {
+        message.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    // success!
+                    Toast.makeText(getApplicationContext(), getString(R.string.success_message), Toast.LENGTH_LONG).show();
+                } else {
+                    // error
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                    builder.setMessage(getString(R.string.error_sending_message))
+                            .setTitle(getString(R.string.error_selecting_file_title))
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
+
+    private ParseObject createMessage() {
+        ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+        message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+        message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+        message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+        message.put(ParseConstants.KEY_FILE_TYPE, mFileType);
+
+        byte[] fileBytes = FileHelper.getByteArrayFromFile(this, mMediaUri);
+
+        if(fileBytes == null) {
+            return null;
+        } else {
+            if(mFileType.equals(ParseConstants.TYPE_IMAGE)) {
+                fileBytes = FileHelper.reduceImageForUpload(fileBytes);
+            }
+
+            String fileName = FileHelper.getFileName(this, mMediaUri, mFileType);
+            ParseFile file = new ParseFile(fileName, fileBytes);
+            message.put(ParseConstants.KEY_FILE, file);
+
+            return message;
+        }
+    }
+
+    protected ArrayList<String> getRecipientIds() {
+        ArrayList<String> recipientIds = new ArrayList<>();
+        ListView ls = (ListView) findViewById(android.R.id.list);
+        for(int i = 0; i < ls.getCount(); i++) {
+            if(ls.isItemChecked(i)) {
+                recipientIds.add(RecipientsFragment.getFriendsID(i));
+            }
+        }
+        return recipientIds;
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class RecipientsFragment extends ListFragment {
+
+        public static final String TAG = RecipientsActivity.class.getSimpleName();
+
+        protected static List<ParseUser> mFriends;
+        protected ParseRelation<ParseUser> mFriendsRelation;
+        protected ParseUser mCurrentUser;
+        protected MenuItem mSendMenuItem;
+
+        public static String getFriendsID(int i) {
+            return mFriends.get(i).getObjectId();
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setHasOptionsMenu(true);
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+
+            mSendMenuItem = menu.getItem(0);
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_recipients, container, false);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            mCurrentUser = ParseUser.getCurrentUser();
+            mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+            ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+            query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+            query.findInBackground(new FindCallback<ParseUser>() {
+                @Override
+                public void done(List<ParseUser> friends, ParseException e) {
+                    if(e == null) {
+                        mFriends = friends;
+
+                        String[] usernames = new String[mFriends.size()];
+
+                        int i = 0;
+                        for (ParseUser user : mFriends) {
+                            usernames[i] = user.getUsername();
+                            i++;
+                        }
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getListView().getContext(),
+                                android.R.layout.simple_list_item_checked,
+                                usernames);
+                        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                        setListAdapter(adapter);
+                    } else {
+                        Log.e(TAG, e.getMessage());
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+                        builder.setMessage(e.getMessage())
+                                .setTitle(R.string.error_title)
+                                .setPositiveButton(android.R.string.ok, null);
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            super.onListItemClick(l, v, position, id);
+
+            if(l.getCheckedItemCount() > 0) {
+                mSendMenuItem.setVisible(true);
+            } else {
+                mSendMenuItem.setVisible(false);
+            }
+        }
+    }
+}
